76c30176e72e24966170fb672d8fe03774d6dfc5
Yonash23 <yonik33333@gmail.com>
2023-05-28 20:21:19 +0300

    Initial Commit

---
+++ SneakEye/SneakEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SneakEyeApp.swift
+//  SneakEye
+//
+//  Created by Йонаш Стан on 28.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SneakEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
